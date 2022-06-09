7e6a27ff4dc9c460d7fd9744aa60ec45a6db3172
minhtam2907 <dominhtam290702@gmail.com>
2022-06-09 18:15:07 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './components/Header';
+import ButtonPluss from './components/Button/ButtonPluss';
+import Input from './components/Input';
+import Divider from './components/Divider';
+import List from './components/List';
+import Panigation from './components/Panigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title={'TO DO LIST APPLICATION'} />
+      <div className='add-task-wrapper'>
+        <Input />
+        <ButtonPluss />
+      </div>
+      <Divider fullWidth />
+      <List />
+      <Divider fullWidth />
+      <Panigation />
+    </div>
+  );
+}
+
+export default App;
